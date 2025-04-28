64635f5836d1328985caae126f2ef7961155a0d1
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-04-28 09:16:48 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true}));
+app.use(cors({ origin: [process.env.FRONTEND_URL, "http://localhost:5173"], credentials: true}));
 
 // Routes
 app.use("/api/auth", authRoutes);
