1852c12746f7e46fb2c158f8d3eef9c95ad29acb
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-04-28 09:14:19 +0530

    Update token file

--- backend/src/utils/generateToken.util.js
+++ backend/src/utils/generateToken.util.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS (1 week)
     httpOnly: true, // Prevent XSS attacks
     sameSite: "strict", // CSRF protection
-    secure: process.env.NODE_ENV !== "development", // Use HTTPS only in production
+    secure: process.env.NODE_ENV === "production", // Use HTTPS only in production
   });
 
   return token;
